813200584951c80374c8a350ffdbaeb0ee847f3a
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-08-22 15:36:00 -0400

    adding code for the triangle character and starting on the pentagon

--- public/script/canvasGame.js
+++ public/script/canvasGame.js
@@ -3,11 +3,15 @@ console.log('sup+++++++++++++++++++++')
 console.dir(canvas)
 var circle = new Circle;
 var square = new Square;
+var triangle = new Triangle;
+var hexagon = new Hexagon;
 
 function draw(){
     ctx.fillStyle = "green";
     circle.create()
     square.create()
+    triangle.create()
+    hexagon.create()
     //where we draw
     // context.drawImage(bgImage, 0,0);
     // context.drawImage(theHero, theHeroLoc.x, theHeroLoc.y);
--- public/script/shapes.js
+++ public/script/shapes.js
@@ -19,3 +19,25 @@ class Square{
         }
     }
 }
+
+class Triangle{
+    constructor() {
+        this.create = (x, y, angle) => {
+            ctx.moveTo(canvas.width/2, canvas.height/2-50);
+            ctx.lineTo(canvas.width/2-50, canvas.height/2+50);
+            ctx.lineTo(canvas.width/2+50, canvas.height/2+50);
+            ctx.fill()
+        }
+    }
+}
+
+class Pentagon{
+    constructor() {
+        this.create = (x, y, angle) => {
+            // ctx.fillRect(100, 100, 100, 100);
+            ctx.strokeRect(100, 100, 100, 100);
+        }
+    }
+}
+
+
