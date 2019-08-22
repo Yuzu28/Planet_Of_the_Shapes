b84bd2e32c40182ae2d4d3f3548252af2d0915a4
Kevin Reid <kevinreid3light@gmail.com>
2019-08-22 16:20:54 -0400

    socket.io inprogress

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var http = require('../bin/www');
+var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/chat', function(req, res, next) {
+  res.render('chat');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 module.exports = router;
