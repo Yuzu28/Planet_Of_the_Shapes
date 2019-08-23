a42f3e3567cb3c5e5ffb8d1cd1cc57d7fbfcfe98
HakeemWashington <hakeemgeorgewashington@gmail.com>
2019-08-23 12:31:17 -0400

    completed shapes drawings

--- public/script/shapes.js
+++ public/script/shapes.js
@@ -4,28 +4,33 @@ const ctx = canvas.getContext('2d');
 
 class Circle{
     constructor() {
-        this.create = (x, y, angle) => {
+        this.create = (x, y, radius, startRadius, endRadius) => {
+            ctx.fillStyle = 'green';
             ctx.beginPath();
-            ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-            ctx.stroke();
+            ctx.arc(x, y, radius, startRadius, endRadius); // Outer circle
+            ctx.fill();
         }
     }
 }
 class Square{
     constructor() {
-        this.create = (x, y, angle) => {
-            // ctx.fillRect(100, 100, 100, 100);
-            ctx.strokeRect(100, 100, 100, 100);
+        this.create = (x, y, width, height) => {
+            ctx.fillStyle = 'red';
+            ctx.fillRect(x, y, width, height)
+            
         }
     }
 }
 
 class Triangle{
     constructor() {
-        this.create = (x, y, angle) => {
-            ctx.moveTo(canvas.width/2, canvas.height/2-50);
-            ctx.lineTo(canvas.width/2-50, canvas.height/2+50);
-            ctx.lineTo(canvas.width/2+50, canvas.height/2+50);
+        this.create = (x, y) => {
+
+            ctx.fillStyle ='blue'
+            ctx.beginPath();
+            ctx.moveTo(x+200,y+50);//x,y values to start at
+            ctx.lineTo(x+150,y+150);//draws a line
+            ctx.lineTo(x+250,y+150)
             ctx.fill()
         }
     }
@@ -33,11 +38,39 @@ class Triangle{
 
 class Pentagon{
     constructor() {
-        this.create = (x, y, angle) => {
-            // ctx.fillRect(100, 100, 100, 100);
-            ctx.strokeRect(100, 100, 100, 100);
+        this.create = (x,y) => {
+ctx.beginPath();
+ctx.moveTo(x+200,y+50);//x,y values to start at
+ctx.lineTo(x+150,y+100);//draws a line
+ctx.moveTo(x+200,y+50)
+ctx.lineTo(x+250,y+100);
+ctx.lineTo(x+250,y+150)
+ctx.lineTo(x+150,y+150)
+ctx.lineTo(x+150,y+100)
+
+ctx.fillStyle="orange";//changed to fill
+ctx.fill()//fill() will only fill in a shape with one gap 
+            
         }
     }
 }
 
+class Hexagon{
+    constructor() {
+        this.create = (x, y) => {
+            ctx.beginPath();
+            ctx.moveTo(x+250,y+50);//x,y values to start at
+            ctx.lineTo(x+300,y+50);//draws a line
+            ctx.lineTo(x+350,y+100)
+            ctx.lineTo(x+300,y+150);
+            ctx.lineTo(x+250,y+150)
+            ctx.lineTo(x+200,y+100)
+            
+            ctx.fillStyle="gold";//changed to fill
+            ctx.fill()//fill() will only fill in a shape with one gap 
+
+            
+        }
+    }
+}
 
