6815cf03cca9e08f2d7cf1a634ed4fe1b883dce9
Kyle Lacy <kylelacy@Kyles-MacBook-Air.local>
2019-08-23 09:54:57 -0400

    Created user login database and register page

---
+++ db.js
@@ -0,0 +1,21 @@
+// Import the dotenv module.
+// Call its `.config()` method
+require('dotenv').config();
+
+const pgp = require('pg-promise')({
+    // Logs SQL queries to the console.
+    query: (e) => console.log(e.query)
+});
+const db = pgp({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_NAME);
+
+console.log('yay you did the thing. wow.');
+
+module.exports = db;
