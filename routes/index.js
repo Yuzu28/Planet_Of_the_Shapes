e5bc17a3a7061a6705384bcc49611a45e282afb9
Charleese Burns <charleeseburns1@yahoo.com>
2019-08-26 13:44:15 -0400

    Added Css and other fixes

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const db = require("../db");
-const bcrypt = require('bcrypt');
-const expressSession = require('express-session');
+const bcrypt = require("bcrypt");
+const expressSession = require("express-session");
 var UserIp;
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+router.get("/register", function(req, res, next) {
+  res.render("register", { title: "Express" });
 });
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
-});
-
-router.get('/game', async function(req, res, next){
-  UserIp = await req.connection.remoteAddress
-  var data = await req
-  console.log(data)
-  console.log(`A User has Joined at: ${UserIp}`)
-  res.render('multiplayer');
+router.get("/login", function(req, res, next) {
+  res.render("login", { title: "Express" });
 });
 
-router.post('/register',(req, res, next)=>{
+router.get("/game", async function(req, res, next) {
+  UserIp = await req.connection.remoteAddress;
+  var data = await req;
+  console.log(data);
+  console.log(`A User has Joined at: ${UserIp}`);
+  res.render("multiplayer");
+});
 
+router.post("/register", (req, res, next) => {
   // const {displayname,password,password2} = req.body;
   const displayname = req.body.displayname;
   const password = req.body.password;
@@ -36,61 +34,61 @@ router.post('/register',(req, res, next)=>{
     SELECT * FROM USERS WHERE DISPLAYNAME = $1 
     
   `;
-  db.any(checkUserExistsQuery, [displayname]).then((results) => {
-    if(results.length > 0){
+  db.any(checkUserExistsQuery, [displayname]).then(results => {
+    if (results.length > 0) {
       // this user alreacy exists
-      res.redirect('/login?msg=userexists'); 
-    }else{
+      res.redirect("/login?msg=userexists");
+    } else {
       // new user.insert
       insertUser();
     }
-  })
-function insertUser(){
-  const insertUserQuery = `INSERT INTO users (displayname,password)
+  });
+  function insertUser() {
+    const insertUserQuery = `INSERT INTO users (displayname,password)
     VALUES
     ($1,$2)
-    returning id`
-    const hash = bcrypt.hashSync(password,10);
-  db.one(insertUserQuery,[displayname,hash]).then((resp)=>{
-    res.json({
-      msg: resp
-    })
-  })
-}
+    returning id`;
+    const hash = bcrypt.hashSync(password, 10);
+    db.one(insertUserQuery, [displayname, hash]).then(resp => {
+      res.json({
+        msg: resp
+      });
+    });
+  }
 
-router.post(`/login`, (req, res, next) =>{
-  res.json(req.body);
-  const checkUserQuery = `
+  router.post(`/login`, (req, res, next) => {
+    res.json(req.body);
+    const checkUserQuery = `
   SELECT * From users WHERE display
   name=$1
   `;
-  const checkUser = db.one(checkUserQuery,[req.body.displayname])
-  const correctPass = bcrypt.compareSync(req.body.pasword, results.password);
-  if(correctPass){
-    // this is a valid user/pass 
-    res.send("Logged In")
-    req.session.displayname = results.displayname;
-    req.session.loggedIn = true;
-    res.redirect('/login');
-    
-  }else{
-    // these arent the droids were looking for
-    res.redirect('/login?msg=badPass')
-  }
-  res.json(results);
-  
-  })
-  checkUser.catch((error) => {
+    const checkUser = db.one(checkUserQuery, [req.body.displayname]);
+    const correctPass = bcrypt.compareSync(req.body.pasword, results.password);
+    if (correctPass) {
+      // this is a valid user/pass
+      res.send("Logged In");
+      req.session.displayname = results.displayname;
+      req.session.loggedIn = true;
+      res.redirect("/login");
+    } else {
+      // these arent the droids were looking for
+      res.redirect("/login?msg=badPass");
+    }
+    res.json(results);
+  });
+  checkUser.catch(error => {
     res.json({
       msg: "userDoesNotExist"
-    })
-  
+    });
+  });
+
+  /* GET users listing. */
+  router.get("/", function(req, res, next) {
+    res.send("respond with a resource");
+  });
 });
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/leaderboard", function(req, res) {
+  res.render("Leaderboard");
 });
-})
-module.exports = {router,UserIp};
+module.exports = { router, UserIp };
