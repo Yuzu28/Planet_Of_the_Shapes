e13bec73c7fcf705b6fdb9386c5d9fabb7b3ddb8
Charleese Burns <charleeseburns1@yahoo.com>
2019-08-26 09:58:57 -0400

    Added leaderboard element

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const db = require("../db");
+const bcrypt = "bcrypt";
+con;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
